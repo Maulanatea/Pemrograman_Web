db1576a2b5cce3a831788a9b2b3393588c16691e
yusup <maulana.md27@gmail.comgit congig --global user.name yusupgit config --global user.email maulana.md27@gmail.com>
2022-06-07 20:27:36 +0700

    tahap satu

---
+++ kuliah/pertemuan 10/latihan1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Mahasiswa</title>
+</head>
+<body>
+        <h3>Daftar Mahasiswa</h3>
+
+        <table border='1' cellpadding='10' cellcpacing='0'>
+            <tr>
+                <th>#</th>
+                <th>Gambar</th>
+                <th>NRP</th>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>Jurusan</th>
+                <th>Aksi</th>
+            </tr>
+            <tr>
+                <th>1</th>
+                <th><img src="img/ikan.jpg" width='60'></th>
+                <th>097865</th>
+                <th>Maulana</th>
+                <th>maul@gmail.com</th>
+                <th>Ekonomi</th>
+                <th>
+                    <a href="">ubah</a> | <a href="">hapus</a>
+                </th>
+            </tr>
+
+        </table>
+</body>
+</html>
