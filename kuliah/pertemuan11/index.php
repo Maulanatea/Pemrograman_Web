231b3a3f5b9fd68c0281b5c46b563fed16ceee6e
yusup <maulana.md27@gmail.comgit congig --global user.name yusupgit config --global user.email maulana.md27@gmail.com>
2022-06-10 08:52:41 +0700

    penambahan form search

--- kuliah/pertemuan11/index.php
+++ kuliah/pertemuan11/index.php
@@ -1,7 +1,14 @@
 <?php 
 require 'functions.php';
 $siswa = query("SELECT * FROM siswa");
+
+//ketika tombol cari di klik
+if (isset($_POST['cari'])) {
+    $siswa = cari($_POST['keyword']);
+}
  ?>
+
+ 
 
 <!DOCTYPE html>
 <html lang="en">
@@ -17,6 +24,12 @@ $siswa = query("SELECT * FROM siswa");
         <a href="tambah.php">Tambah Data Siswa</a>
         <br><br>
 
+        <form action="" method="POST">
+            <input type="text" name="keyword" placeholder="cari atuu.." autocomplete="off" autofocus >
+            <button type="submit" name="cari" >cari</button>
+        </form>
+        <br>
+
         <table border='1' cellpadding='10' cellspacing='0'>
             <tr>
                 <th>#</th>
@@ -24,6 +37,14 @@ $siswa = query("SELECT * FROM siswa");
                 <th>Nama</th>
                 <th>Aksi</th>
             </tr>
+
+            <?php if(empty($siswa)) : ?>
+            <tr>
+                <td colspan="4" >
+                   <p style="color: red; font-style: italic;"> Data Siswa Tidak ditemukan...!!</p>
+                </td>
+            </tr>
+            <?php endif; ?>
             
             <?php $i = 1;
             foreach( $siswa as $s ) : ?>
